ac9b5a5153db583a4e075aedd925bf5c4c4d3fa8
Gaetz <gaetz@rufflerim.com>
2017-10-20 12:01:30 +0200

    Helicopter call + debug (scripts)

---
+++ Assets/Scripts/ClearArea.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearArea : MonoBehaviour
+{
+
+    public float timeSinceLastTrigger = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastTrigger += Time.deltaTime;
+        if (timeSinceLastTrigger > 1f && Time.realtimeSinceStartup > 10f)
+        {
+            SendMessageUpwards("OnFindClearArea");
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag != "Player")
+        {
+            timeSinceLastTrigger = 0f;
+        }
+    }
+}
